0dc2c255b542bf21cd2aeb85132984e3a3014db9
Ritesh Jagga <riteshjagga@gmail.com>
2025-11-20 18:30:00 +0530

    Add Vite, Typescript, and TailwindCSS support

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react';
-import { createRoot } from 'react-dom/client';
-import Counter from './Counter/Counter';
+import { useState } from 'react'
+import Counter from './Counter/Counter'
+import './App.css'
 
 const App = (): React.ReactElement => {
-  const [count, setCount] = useState<number>(0);
+  const [count, setCount] = useState<number>(0)
 
   const handleDecrement = () => {
-    setCount(count - 1);
-  };
+    setCount(count - 1)
+  }
 
   const handleIncrement = () => {
-    setCount(count + 1);
-  };
+    setCount(count + 1)
+  }
 
   return (
     <>
       <h1>React Typescript Webpack Template</h1>
-      <Counter
-        value={count}
-        onDecrement={handleDecrement}
-        onIncrement={handleIncrement}
-      />
+      <Counter value={count} onDecrement={handleDecrement} onIncrement={handleIncrement} />
     </>
-  );
-};
-
-const app = document.getElementById('app');
-if (!app) {
-  throw new Error('Missing root element with app id.');
+  )
 }
-const appRoot = createRoot(app);
-appRoot.render(<App />);
+
+export default App
